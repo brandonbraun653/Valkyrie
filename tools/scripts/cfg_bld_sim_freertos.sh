5cde91883bfdf5304d62e9227a7d370a519e4104
Brandon Braun <brandonbraun653@gmail.com>
2021-03-29 16:28:40 -0500

    Add tasks to clean/configure/build

--- tools/scripts/cfg_bld_sim_freertos.sh
+++ tools/scripts/cfg_bld_sim_freertos.sh
@@ -1,1 +1,9 @@
+
+# Move up to the repo root
+cd ../../
+
+# Create the build directory
+mkdir -p ./build
+cd ./build
+
 cmake -DTOOLCHAIN=gcc -DFREERTOS_THREADS=ON -DCUSTOM_FREERTOS_CFG=ON -DDEVICE_TARGET=sim -DCMAKE_BUILD_TYPE=Debug ./../
--- tools/scripts/cfg_bld_sim_os.sh
+++ tools/scripts/cfg_bld_sim_os.sh
@@ -1,6 +1,5 @@
 
 mkdir -p ./build
 cd ./build
-rm -rf ./*
 
 cmake -DTOOLCHAIN=gcc -DNATIVE_THREADS=ON -DDEVICE_TARGET=sim -DCMAKE_BUILD_TYPE=Debug ./../
