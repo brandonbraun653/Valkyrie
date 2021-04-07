237c1c73e679dc9c08ef28079255054e433961f9
Brandon Braun <brandonbraun653@gmail.com>
2021-04-07 12:27:10 -0500

    Verified data path from sim to project registry

--- src/kernel/threads/monitor_thread.cpp
+++ src/kernel/threads/monitor_thread.cpp
@@ -9,12 +9,14 @@
  *******************************************************************************/
 
 /* Aurora Includes */
+#include <Aurora/sensors>
 #include <Aurora/logging>
 
 /* Chimera Includes */
 #include <Chimera/thread>
 
 /* Valkyrie Includes */
+#include <Valkyrie/kernel>
 #include <src/kernel/threads/thread_listing.hpp>
 
 namespace Valkyrie::Thread::SystemMonitor
@@ -25,8 +27,17 @@ namespace Valkyrie::Thread::SystemMonitor
   void main( void *arg )
   {
     LOG_INFO( "Monitor thread startup\r\n" );
+
+    Aurora::Sensor::TriAxisSample sensorData;
+
     while( true )
     {
+      if( Registry::readSafe( Registry::KEY_SENSOR_DATA_ACCEL, &sensorData, sizeof( sensorData ) ) )
+      {
+        LOG_DEBUG( "Accel: [x:%2.2f, y:%2.2f, z:%2.2f]\r\n", sensorData.x, sensorData.y, sensorData.z );
+      }
+
+
       Chimera::delayMilliseconds( 100 );
     }
   }
