73e08b01498bb1878af0ccab1941e224ad330bac
Brandon Braun <brandonbraun653@gmail.com>
2021-08-15 09:14:53 -0700

    pull up submodules

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -6,5 +6,6 @@ cmake_minimum_required(VERSION 3.18.0)
 # ====================================================
 set(PROJECT_ROOT "${CMAKE_CURRENT_SOURCE_DIR}" CACHE STRING "")
 set(COMMON_TOOL_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/lib/CommonTools" CACHE STRING "")
+set(BOOST_ROOT "$ENV{HOME}/Programs/boost/boost_1_76_0" CACHE STRING "")
 
 include("${COMMON_TOOL_ROOT}/cmake/options/toolchain.cmake")
